6aae5aa7e33794529e9a8462dea8772a4782e13b
Rahul Patil <rahulpp@live.com>
2018-01-21 17:39:33 +0530

    Basic cli setup

---
+++ stackr/src/components/Child.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    Child router
+  </div>
+</template>
+
+<script>
+export default {
+  name : 'child'
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ stackr/src/components/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="test">
+    <p>Hello from home</p>
+    <p>{{ newVar }}</p>
+    <button v-on:click="clickMe">Click Me</button>
+    <child></child>
+  </div>
+</template>
+
+<script>
+import Child from '@/components/Child';
+export default {
+  name: 'Home',
+  components: {
+    'child' : Child
+  },
+  data () {
+    return {
+      msg: 'Stackr Home',
+      newVar: new Date().toString()
+    }
+  },
+  methods: {
+      clickMe: function () {
+        this.newVar = (new Date()).toString()
+      }
+  }
+}
+</script>
